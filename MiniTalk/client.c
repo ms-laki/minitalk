ebff178c3188b30225c1e18b736e044ce770ecd6
Lucky <87255552+ms-laki@users.noreply.github.com>
2022-06-13 21:53:20 +0930

    Draft 1
    Work in progress - Has the below error message:
    
    server.c:17:10: fatal error: 'ft_printf.h' file not found
    #include "ft_printf.h"

---
+++ MiniTalk/client.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgiirre <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 18:50:17 by lgiirre           #+#    #+#             */
+/*   Updated: 2022/06/13 21:21:27 by lgiirre          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include "ft_printf.h"
+
+void	send_bit(int pid, char *str, size_t len)
+{
+	int		shift;
+	size_t	i;
+
+	i = 0;
+	while (i <= len)
+	{
+		shift = 0;
+		while (shift < 7)
+		{
+			if ((str[i] >> shift) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			shift++;
+			usleep(300);
+		}
+		i++;
+	}
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	length;
+
+	length = 0;
+	while (*s++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+int	ft_atoi(const char *str)
+{
+	long	new;
+	long	neg;
+	size_t	c;
+
+	c = 0;
+	new = 0;
+	neg = 1;
+	while (str[c] == ' ' || (str[c] >= '\t' && str[c] <= '\r'))
+	{
+		c++;
+	}
+	if (str[c] == '+' || str[c] == '-')
+	{
+		if (str[c] == '-' )
+		{
+			neg = neg * -1;
+		}
+		c++;
+	}
+	while (str[c] >= '0' && str[c] <= '9')
+	{
+		new = (str[c] - '0') + (new * 10);
+		c++;
+	}
+	return (new * neg);
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*str;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		str = argv[2];
+		send_bit(pid, str, ft_strlen(str));
+	}
+	else
+		ft_printf("\nYOU EITHER LEFT IT BLANK OR ARE DOING MORE THAN 1 WORD\n\n");
+}
---
+++ MiniTalk/server.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgiirre <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 18:50:46 by lgiirre           #+#    #+#             */
+/*   Updated: 2022/06/13 21:50:57 by lgiirre          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include "ft_printf.h"
+
+typedef struct s_message
+{
+	char	c;
+	size_t	i;
+}	t_message;
+
+t_message	g_msg;
+
+void	bit_handler(int bit)
+{
+	int	i;
+
+	i = 0;
+	g_msg.c += ((bit & 1) << g_msg.i);
+	g_msg.i++;
+	if (g_msg.i == 7)
+	{
+		ft_printf("%c", g_msg.c);
+		if (!g_msg.c)
+			ft_printf("\n");
+		g_msg.c = 0;
+		g_msg.i = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_printf("Welcome To Lucky's Server!\n");
+	ft_printf("My Server PID is: %d\n", getpid());
+	while (1)
+	{
+		signal(SIGUSR2, bit_handler);
+		signal(SIGUSR1, bit_handler);
+		pause();
+	}
+	return (0);
+}
